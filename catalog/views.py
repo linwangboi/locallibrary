01d1ee25bcbe7450b3a32ea9226deb7f1ad803a0
linwangboi <linwangboi@gmail.com>
2024-11-30 07:21:43 +0000

    added dl, dt, dd for author detail view

--- catalog/views.py
+++ catalog/views.py
@@ -16,11 +16,16 @@ def index(request):
 
     # the all() is implied by default.
     num_authors = Author.objects.count()
+    # Number of visits to this view
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
-        'num_authors': num_authors
+        'num_authors': num_authors,
+        'num_visits': num_visits,
     }
     return render(request, 'index.html', context=context)
 
