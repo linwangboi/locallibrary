3aeeca848a3961c7ee5ef82fed7b5781dfdfaa3f
linwangboi <linwangboi@gmail.com>
2024-12-02 07:28:35 +0000

    added borrower field for bookinstance

--- catalog/models.py
+++ catalog/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.urls import reverse
 from django.db.models import UniqueConstraint
 from django.db.models.functions import Lower
-
+from django.conf import settings
+from datetime import date
 # Create your models here.
 
 class Genre(models.Model):
@@ -74,6 +75,7 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
     LOAN_STATUS = (
         ('m', 'Maintenance'),
         ('o', 'On loan'),
@@ -91,6 +93,10 @@ class BookInstance(models.Model):
         ordering=['due_back']
     def __str__(self):
         return f'{self.id} ({self.book.title})'
+    @property
+    def is_overdue(self):
+        """Determines if the book is overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back)
 
 
 class Author(models.Model):
